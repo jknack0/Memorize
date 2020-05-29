489cfbe6ccc578692b8e4324644e5373edfa735b
Jonathon Knack <jknack@mail.sfsu.edu>
2020-05-29 12:57:29 -0700

    initial commit

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,40 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Jon Knack on 5/28/20.
+//  Copyright © 2020 Jon Knack. All rights reserved.
+//
+
+import SwiftUI
+
+func createCardContent(pairIndex: Int) -> String {
+    return "🧐"
+}
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["🧐", "👹", "👾"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    
+    // MARK: - Access to the Model
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
+
+struct EmojiMemoryGame_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
---
+++ MemoryGame.swift
@@ -0,0 +1,33 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Jon Knack on 5/28/20.
+//  Copyright © 2020 Jon Knack. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+    
+    func choose(card: Card) {
+        print("card chose: \(card)")
+    }
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = Array<Card>()
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content: CardContent = cardContentFactory(pairIndex)
+            cards.append(Card(content: content, id: pairIndex * 2))
+            cards.append(Card(content: content, id: pairIndex * 2 + 1))
+        }
+    }
+    
+    struct Card: Identifiable{
+        var isFaceUp: Bool = true
+        var isMatched: Bool = false
+        var content: CardContent
+        var id: Int
+    }
+}
